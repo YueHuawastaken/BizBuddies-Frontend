e2232eab88c2877361b3cedf177e274e931272b7
lurbh <lurbh@hotmail.com>
2024-06-20 15:46:40 +0000

    fis bug when product has no version

--- 01-basics/src/pages/productListing.js
+++ 01-basics/src/pages/productListing.js
@@ -64,30 +64,34 @@ export default function ProductListing (){
                 <>
                      <Container fluid className="ms-2">
                         <Row xs={1} s={2} md={2} lg={3} xl={4} xxl={5} style={{justifyContent:'flex-start'}}>
-                            {productsData.map(product =>  
-                                <Col style={{marginLeft:'0px'}} key={product.id}>
-                                    <Card style={{ width: '18rem', marginTop: '10px', marginBottom:'20px'}}>
-                                    <Card.Img variant="top" src={product.productVersion[0].image_url} style={{ minHeight: '220px', maxHeight:'220px', objectFit:'contain'}}/>
-                                    <Card.Body>
-                                        <Card.Title>{product.productName}</Card.Title>
-                                        <Card.Text className="mb-1">
-                                        {product.description}
-                                        </Card.Text>
-                                        <div className="mb-2">
-                                            {product.productVersion.map((productVersion)=> (
+                            {productsData.map(product =>  (
+                                <>
+                                {
+                                    product.productVersion.length !== 0 && <Col style={{marginLeft:'0px'}} key={product.id}>
+                                        <Card style={{ width: '18rem', marginTop: '10px', marginBottom:'20px'}}>
+                                        <Card.Img variant="top" src={product.productVersion[0].image_url} style={{ minHeight: '220px', maxHeight:'220px', objectFit:'contain'}}/>
+                                        <Card.Body>
+                                            <Card.Title>{product.productName}</Card.Title>
+                                            <Card.Text className="mb-1">
+                                            {product.description}
+                                            </Card.Text>
+                                            <div className="mb-2">
+                                                {product.productVersion.map((productVersion)=> (
+                                                    
+                                                    <span key={productVersion.versionName}>
+                                                        <Badge bg="secondary">{productVersion.price}</Badge>
+                                                    </span>
                                                 
-                                                <span key={productVersion.versionName}>
-                                                    <Badge bg="secondary">{productVersion.price}</Badge>
-                                                </span>
-                                            
-                                            ))}
-                                        </div>
-                                        <Link to={`/products/${product.id}`} element={<ProductDetails />}>
-                                            <Button variant="dark"> See Details </Button>
-                                        </Link>
-                                    </Card.Body>
-                                    </Card>             
-                                </Col>
+                                                ))}
+                                            </div>
+                                            <Link to={`/products/${product.id}`} element={<ProductDetails />}>
+                                                <Button variant="dark"> See Details </Button>
+                                            </Link>
+                                        </Card.Body>
+                                        </Card>             
+                                    </Col>
+                            }
+                                </>)
                             )}
                         </Row>
                     </Container>
